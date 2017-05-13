13e15ae73ba7344acb66cf455aefd2ded54c2f31
TinaFeng <xuanfeng1996@gmail.com>
2017-05-13 16:20:18 -0700

    Construction in progreaa

---
+++ CS 190 PsychoPath/Assets/Hunter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hunter : MonoBehaviour {
+
+ 
+    // Use this for initialization
+    void Start () {
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("I've got you in my sight.");
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+
+
+    }
+}
