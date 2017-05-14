cbdc8c02f722c06e2290168438477a4ea6bf6a00
Alex561 <alexchapp@yahoo.com>
2017-05-13 17:03:47 -0700

    test unmerge

---
+++ CS 190 PsychoPath/Assets/Scripts/start_menu_script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class start_menu_script : MonoBehaviour {
+    public string scene_name;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void NextScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scene_name);
+    }
+}
