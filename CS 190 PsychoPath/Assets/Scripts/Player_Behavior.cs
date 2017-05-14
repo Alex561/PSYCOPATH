2ead3606d9b66bc70b96af568f0410e2f85f73f0
TinaFeng <xuanfeng1996@gmail.com>
2017-05-13 19:29:53 -0700

    Prototype 1.2

--- CS 190 PsychoPath/Assets/Scripts/Player_Behavior.cs
+++ CS 190 PsychoPath/Assets/Scripts/Player_Behavior.cs
@@ -6,10 +6,14 @@ public class Player_Behavior : MonoBehaviour {
 
     bool can_Hide = false;
 
+    int counter = 0;
+    GameObject frontWall;
+    GameObject[] monsters;
 	// Use this for initialization
 	void Start () {
 
         Sprite player = this.GetComponent<SpriteRenderer>().sprite;
+        frontWall = GameObject.FindGameObjectWithTag("FrontWall");
 		
 	}
 
@@ -26,6 +30,7 @@ public class Player_Behavior : MonoBehaviour {
 
             GameObject clown = Instantiate(Resources.Load("clown"), new Vector2(Random.Range(this.transform.position.x + 10.0f, this.transform.position.x + 10.0f * -1),Random.Range(this.transform.position.y + 10.0f, this.transform.position.y + 10.0f * -1)),Quaternion.identity) as GameObject;
             Destroy(collision.gameObject);
+            counter += 1;
         }
 
         if (collision.tag == "Clown")
@@ -36,12 +41,16 @@ public class Player_Behavior : MonoBehaviour {
                 collision.GetComponent<Vision>().chase = true;
             }
             }
+        if (collision.tag == "Exit")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("ClownDeath");
+        }
 
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if( collision.gameObject.tag == "Clown")
+        if( collision.gameObject.tag == "Obstacle")
         {
 
             UnityEngine.SceneManagement.SceneManager.LoadScene("ClownDeath");
@@ -69,17 +78,48 @@ public class Player_Behavior : MonoBehaviour {
             {
                 this.GetComponent<SpriteRenderer>().enabled = true;
                 this.GetComponent<PlayerMovement>().hide = false;
+
+                monsters = GameObject.FindGameObjectsWithTag("Obstacle");
+                for (int i = 0; i != monsters.Length; i++)
+                {
+                    monsters[i].GetComponent<Collider2D>().enabled = true;
+                }
             }
-
-
+        }
+        if (counter == 6)
+        {
+            endGame();
         }
 
 	}
+    
+    void endGame()
+    {
+        monsters = GameObject.FindGameObjectsWithTag("Clown");
+        for(int i = 0; i != monsters.Length; i++)
+        {
+            monsters[i].GetComponent<Vision>().chase = true;
+        }
+        Destroy(frontWall);
+    }
 
     void hide()
     {
         this.GetComponent<SpriteRenderer>().enabled = false;
         this.GetComponent<PlayerMovement>().hide = true;
         this.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+
+        monsters = GameObject.FindGameObjectsWithTag("Clown");
+        for (int i = 0; i != monsters.Length; i++)
+        {
+            monsters[i].GetComponent<Vision>().chase = false;
+        }
+
+        monsters = GameObject.FindGameObjectsWithTag("Obstacle");
+        for (int i = 0; i != monsters.Length; i++)
+        {
+            monsters[i].GetComponent<Collider2D>().enabled = false;
+        }
+
     }
 }
