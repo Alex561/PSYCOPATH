f91ece7d2f885186f91e7d1e843803c74d2e213e
Allisah Solorio <allisahs@uci.edu>
2017-05-18 15:16:53 -0700

    wwise integration

---
+++ CS 190 PsychoPath/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,44 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMB_CAROUSEL = 4103674448U;
+        static const AkUniqueID CLOWN = 1384162296U;
+        static const AkUniqueID DEATH_CLOWN = 1871169887U;
+        static const AkUniqueID DEATH_DOLL = 1011886111U;
+        static const AkUniqueID DEATH_ROBOT = 2276324068U;
+        static const AkUniqueID DOLL = 1894625214U;
+        static const AkUniqueID ICECREAMTRUCK = 2816637309U;
+        static const AkUniqueID MUSIC_BG_GAME = 284011213U;
+        static const AkUniqueID MUSICBOX = 2785641797U;
+        static const AkUniqueID ROBOT = 1930240631U;
+        static const AkUniqueID WIN = 979765101U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID DEATH = 779278001U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
